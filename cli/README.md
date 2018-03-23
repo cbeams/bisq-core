0756c57c974226f4ac2209b35f0d61674d4a8c1a
Chris Beams <chris@beams.io>
2018-03-23 15:55:07 +0100

    Add cli subproject WIP

---
+++ cli/README.md
@@ -0,0 +1,6 @@
+
+
+declare -A issue
+
+https://stackoverflow.com/questions/27002192/declare-a-bash-does-not-work-on-mac-os-x
+https://stackoverflow.com/questions/453236/how-to-set-my-default-shell-on-mac
---
+++ cli/src/main/java/bisq/cli/Bisq.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.cli;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.HelpCommand;
+
+@Command(
+    name = "bisq",
+    version = "0.1.0",
+    mixinStandardHelpOptions = true,
+
+    subcommands = {
+        HelpCommand.class,
+        BisqBalance.class
+    }
+)
+public class Bisq implements Runnable {
+
+    @Override
+    public void run() {
+        CommandLine.usage(this, System.out);
+    }
+}
+
+@Command(
+    name = "balance"
+)
+class BisqBalance implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("0.00000001");
+    }
+}
+
